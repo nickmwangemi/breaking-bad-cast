6d85538b7c28b55a4c56060a51786f2d46c36042
nickmwangemi <nickmwangemi@gmail.com>
2020-09-27 12:49:17 +0300

    Initial commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt="Breaking Bad Logo" />
+        </header>
+    )
+}
+
+export default Header
+
